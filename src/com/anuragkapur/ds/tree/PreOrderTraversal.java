67608fba6ac1b7cafa96160882749bb04ed40045
Anurag Kapur <anuragkapur@gmail.com>
2014-05-09 05:59:34 +0100

    added preorder tree traversal

---
+++ src/com/anuragkapur/ds/tree/PreOrderTraversal.java
@@ -0,0 +1,24 @@
+package com.anuragkapur.ds.tree;
+
+/**
+ * @author: anuragkapur
+ * @since: 09/05/2014
+ */
+
+public class PreOrderTraversal {
+
+    private static void preOrder(TreeNode startingNode) {
+
+        if (startingNode != null) {
+            System.out.println(startingNode.getPayload());
+            preOrder(startingNode.getLeft());
+            preOrder(startingNode.getRight());
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeNode rootNode = BalancedTreeFactory.getBalancedBinaryTree();
+        preOrder(rootNode);
+    }
+
+}
