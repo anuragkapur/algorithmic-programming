500aa5d07df7262d552d37896ca06e899e2c239d
Anurag Kapur <anuragkapur@gmail.com>
2014-06-10 00:17:45 +0100

    implemented duplicate removal without using temporary storage buffer. run time is quadratic

---
+++ src/com/anuragkapur/ctci/Prob2_1_1_UnsortedLinkedList.java
@@ -0,0 +1,38 @@
+package com.anuragkapur.ctci;
+
+import com.anuragkapur.ds.linkedlist.SinglyLinkedListNode;
+
+/**
+ * @author: anuragkapur
+ * @since: 10/06/14
+ */
+
+public class Prob2_1_1_UnsortedLinkedList {
+
+    /**
+     * Running time = O(n^2)
+     *
+     * @param singlyLinkedListNode
+     * @return
+     */
+    public static SinglyLinkedListNode removeDuplicates(SinglyLinkedListNode singlyLinkedListNode) {
+
+        System.out.println("will remove duplicates");
+
+        SinglyLinkedListNode dedupedListNode = singlyLinkedListNode;
+        SinglyLinkedListNode currentNode;
+
+        while (dedupedListNode != null) {
+            currentNode = dedupedListNode;
+            while (currentNode != null && currentNode.getNext() != null) {
+                if (currentNode.getNext().getPayload().equals(dedupedListNode.getPayload())) {
+                    currentNode.setNext(currentNode.getNext().getNext());
+                }
+                currentNode = currentNode.getNext();
+            }
+            dedupedListNode = dedupedListNode.getNext();
+        }
+
+        return singlyLinkedListNode;
+    }
+}
