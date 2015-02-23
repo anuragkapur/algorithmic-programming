94da2aff6c594c41161fd27eb84e51c9bdb48842
Anurag Kapur <anuragkapur@gmail.com>
2015-02-23 23:11:37 +0000

    added note on complexity

--- src/com/anuragkapur/ctci/linkedlists/Prob2_7_Palindrome.java
+++ src/com/anuragkapur/ctci/linkedlists/Prob2_7_Palindrome.java
@@ -9,6 +9,16 @@ import java.util.Stack;
  */
 public class Prob2_7_Palindrome {
 
+    /**
+     * Check is list is palindrome
+     *
+     * Run time complexity: O(n), 2 iterations of the list - one to determine size and other to check if palindrome
+     * using a stack
+     * Space complexity: O(n/2) = O(n), a stack with capacity of half the list is used.
+     *
+     * @param head
+     * @return
+     */
     public static boolean isPalindrome(LinkedListNode<Integer> head) {
 
         LinkedListNode<Integer> current = head;
