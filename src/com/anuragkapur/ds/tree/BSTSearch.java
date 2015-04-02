30edc46e0050dcb678a516c44b8c4894b81fc738
Anurag Kapur <anuragkapur@gmail.com>
2015-04-02 01:22:24 +0100

    Added note on runtime complexity of BST search.

--- src/com/anuragkapur/ds/tree/BSTSearch.java
+++ src/com/anuragkapur/ds/tree/BSTSearch.java
@@ -5,6 +5,13 @@ package com.anuragkapur.ds.tree;
  */
 public class BSTSearch {
 
+    /**
+     * Running time: O(h) = O(log n), where h is height of tree and n is the number of nodes in the tree
+     *
+     * @param searchKey
+     * @param rootNode
+     * @return
+     */
     public TreeNode search(int searchKey, TreeNode rootNode) {
 
         if (rootNode == null || rootNode.getKey() == searchKey) {
