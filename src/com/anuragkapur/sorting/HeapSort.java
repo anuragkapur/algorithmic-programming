eaa0e61d6d1076e8ad19964c199d7903a115aef6
Anurag Kapur <anurag@beancrunch.com>
2015-04-09 17:13:54 +0100

    Refactored heap sort to use max-heapify instead of building a new max-heap for each element. Also added info on run time complexity.

--- src/com/anuragkapur/sorting/HeapSort.java
+++ src/com/anuragkapur/sorting/HeapSort.java
@@ -5,23 +5,43 @@ package com.anuragkapur.sorting;
  */
 public class HeapSort {
 
+    /**
+     * Running time: O(n log n)
+     * @param a
+     */
     public void sort(int a[]) {
 
-        for (int i=0; i<a.length; i++) {
-            int heapSize = a.length - i;
-            buildMaxHeap(a, heapSize);
+        int heapSize = a.length - 1;
+        buildMaxHeap(a, heapSize);
+
+        for (int i=a.length-1; i>0; i--) {
             int max = a[0];
-            a[0] = a[heapSize-1];
-            a[heapSize-1] = max;
+            a[0] = a[i];
+            a[i] = max;
+
+            maxHeapify(a, 0, heapSize--);
         }
     }
 
+    /**
+     * Running time: O(n log n) - correct but not tight enough, O(n) - asymptotically tight upper bound.
+     *
+     * @param a
+     * @param heapSize
+     */
     private void buildMaxHeap(int a[], int heapSize) {
         for (int i=(heapSize/2)-1; i>=0; i--) {
             maxHeapify(a, i, heapSize);
         }
     }
 
+    /**
+     * Running time: O(log n) where n is the number of elements in the tree rooted at i.
+     *
+     * @param a
+     * @param i
+     * @param heapSize
+     */
     private void maxHeapify(int a[], int i, int heapSize) {
         int left = left(i, a, heapSize);
         int right = right(i, a, heapSize);
