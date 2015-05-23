07ba37e00c861393f0eec31a501972572a176e5c
Anurag Kapur <anurag@beancrunch.com>
2015-05-23 13:20:16 +0100

    Updating inorder traversal to records traversal in a List for future inspection in addition to simply printing on console.

--- src/com/anuragkapur/ds/tree/InOrderTraversal.java
+++ src/com/anuragkapur/ds/tree/InOrderTraversal.java
@@ -1,5 +1,8 @@
 package com.anuragkapur.ds.tree;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * @author: anuragkapur
  * @since: 09/05/2014
@@ -7,17 +10,25 @@ package com.anuragkapur.ds.tree;
 
 public class InOrderTraversal {
 
-    public static void inOrder(TreeNode startingNode) {
+    private List<Integer> result = new LinkedList<>();
+
+    public void inOrder(TreeNode startingNode) {
 
         if (startingNode != null) {
             inOrder(startingNode.getLeft());
+            result.add(startingNode.getKey());
             System.out.println(startingNode.getKey());
             inOrder(startingNode.getRight());
         }
     }
 
+    public List<Integer> getResult() {
+        return result;
+    }
+
     public static void main(String[] args) {
         TreeNode rootNode = TreeFactory.getBalancedBinaryTree();
-        inOrder(rootNode);
+        InOrderTraversal inOrderTraversal = new InOrderTraversal();
+        inOrderTraversal.inOrder(rootNode);
     }
 }
