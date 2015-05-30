c2638bcee560a8af095917a555b64bc09a0a5c1d
Anurag Kapur <anurag@beancrunch.com>
2015-05-30 18:51:16 +0100

    Add Height of binary tree algo

---
+++ src/com/anuragkapur/pie/teesandgraphs/HeightOfBinaryTree.java
@@ -0,0 +1,19 @@
+package com.anuragkapur.pie.teesandgraphs;
+
+/**
+ * @author anuragkapur
+ */
+public class HeightOfBinaryTree {
+
+    public int getHeight(Node<Integer> root) {
+
+        if (root == null || (root.getLeft() == null && root.getRight() == null)) {
+            return 0;
+        } else {
+            int left = getHeight(root.getLeft());
+            int right = getHeight(root.getRight());
+
+            return Math.max(left, right) + 1;
+        }
+    }
+}
---
+++ test/com/anuragkapur/pie/teesandgraphs/HeightOfBinaryTreeTest.java
@@ -0,0 +1,46 @@
+package com.anuragkapur.pie.teesandgraphs;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author anuragkapur
+ */
+public class HeightOfBinaryTreeTest {
+
+    HeightOfBinaryTree heightOfBinaryTree = new HeightOfBinaryTree();
+
+    @Test
+    public void testGetHeight1() throws Exception {
+        assertEquals(0, heightOfBinaryTree.getHeight(null));
+    }
+
+    @Test
+    public void testGetHeight2() throws Exception {
+        Node<Integer> node = new Node<>();
+        assertEquals(0, heightOfBinaryTree.getHeight(node));
+    }
+
+    @Test
+    public void testGetHeight3() throws Exception {
+        Node<Integer> node = new Node<>();
+        Node<Integer> node1 = new Node<>();
+        Node<Integer> node2 = new Node<>();
+        node.setLeft(node1);
+        node.setRight(node2);
+        assertEquals(1, heightOfBinaryTree.getHeight(node));
+    }
+
+    @Test
+    public void testGetHeight4() throws Exception {
+        Node<Integer> node = new Node<>();
+        Node<Integer> node1 = new Node<>();
+        Node<Integer> node2 = new Node<>();
+        Node<Integer> node3 = new Node<>();
+        node.setRight(node1);
+        node1.setLeft(node2);
+        node1.setRight(node3);
+        assertEquals(2, heightOfBinaryTree.getHeight(node));
+    }
+}
