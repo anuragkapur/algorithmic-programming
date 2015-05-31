0715fb5dfeefea184a80800b0f2e5db9059bfd72
Anurag Kapur <anurag@beancrunch.com>
2015-05-31 14:13:12 +0100

    Add inorder traversal algo for a binary tree

---
+++ src/com/anuragkapur/pie/teesandgraphs/Inorder.java
@@ -0,0 +1,28 @@
+package com.anuragkapur.pie.teesandgraphs;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author anuragkapur
+ */
+public class Inorder {
+
+    public List<Integer> inorder(Node<Integer> root) {
+        return inorder(root, null);
+    }
+
+    private List<Integer> inorder(Node<Integer> root, List<Integer> result) {
+        if (root != null) {
+            if (result == null) {
+                result = new ArrayList<>();
+            }
+
+            inorder(root.getLeft(), result);
+            result.add(root.getData());
+            inorder(root.getRight(), result);
+        }
+
+        return result;
+    }
+}
---
+++ test/com/anuragkapur/pie/teesandgraphs/InorderTest.java
@@ -0,0 +1,62 @@
+package com.anuragkapur.pie.teesandgraphs;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author anuragkapur
+ */
+public class InorderTest {
+
+    private Inorder inorder = new Inorder();
+
+    @Test
+    public void testInorder1() throws Exception {
+
+        assertNull(inorder.inorder(null));
+    }
+
+    @Test
+    public void testInorder2() throws Exception {
+
+        Node<Integer> root = new Node<>(10);
+        assertEquals("[10]", inorder.inorder(root).toString());
+    }
+
+    @Test
+    public void testInorder3() throws Exception {
+
+        Node<Integer> root = new Node<>(4);
+        Node<Integer> node2 = new Node<>(2);
+        Node<Integer> node6 = new Node<>(6);
+        Node<Integer> node1 = new Node<>(1);
+        Node<Integer> node3 = new Node<>(3);
+        Node<Integer> node5 = new Node<>(5);
+        Node<Integer> node7 = new Node<>(7);
+
+        root.setLeft(node2);
+        root.setRight(node6);
+        node2.setLeft(node1);
+        node2.setRight(node3);
+        node6.setLeft(node5);
+        node6.setRight(node7);
+
+        assertEquals("[1, 2, 3, 4, 5, 6, 7]", inorder.inorder(root).toString());
+    }
+
+    @Test
+    public void testInorder4() throws Exception {
+
+        Node<Integer> root = new Node<>(4);
+        Node<Integer> node2 = new Node<>(2);
+        Node<Integer> node1 = new Node<>(1);
+        Node<Integer> node3 = new Node<>(3);
+
+        root.setLeft(node2);
+        node2.setLeft(node1);
+        node2.setRight(node3);
+
+        assertEquals("[1, 2, 3, 4]", inorder.inorder(root).toString());
+    }
+}
