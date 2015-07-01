37295f744de8fdaafaa98a6aaa201e21d0fc946f
Anurag Kapur <anurag@beancrunch.com>
2015-07-02 00:58:51 +0100

    Add another alternate solution to problem #217 using Java 8 streams

--- src/com/anuragkapur/leetcode/ContainsDuplicate.java
+++ src/com/anuragkapur/leetcode/ContainsDuplicate.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.OptionalInt;
 import java.util.Set;
+import java.util.stream.IntStream;
 
 /**
  * @author anuragkapur
@@ -26,5 +27,12 @@ public class ContainsDuplicate {
         Set<Integer> allNums = new HashSet<>();
         OptionalInt duplicateCount = Arrays.stream(nums).filter(num -> !allNums.add(num)).findAny();
         return duplicateCount.isPresent();
+    }
+
+    public boolean containtDuplicateAlternate2(int [] nums) {
+        IntStream distinctNumStream = Arrays.stream(nums).distinct();
+        int distinctCount = (int)distinctNumStream.count();
+
+        return (distinctCount != nums.length);
     }
 }
--- test/com/anuragkapur/leetcode/ContainsDuplicateTest.java
+++ test/com/anuragkapur/leetcode/ContainsDuplicateTest.java
@@ -15,14 +15,17 @@ public class ContainsDuplicateTest {
     public void testContainsDuplicate() throws Exception {
         assertEquals(true, containsDuplicate.containsDuplicate(new int[]{1, 2, 3, 4, 5, 3}));
         assertEquals(true, containsDuplicate.containtDuplicateAlternate(new int[]{1, 2, 3, 4, 5, 3}));
+        assertEquals(true, containsDuplicate.containtDuplicateAlternate2(new int[]{1, 2, 3, 4, 5, 3}));
 
         assertEquals(true, containsDuplicate.containsDuplicate(new int[]{5, 3, 1, 4, 3, 2}));
         assertEquals(true, containsDuplicate.containtDuplicateAlternate(new int[]{5, 3, 1, 4, 3, 2}));
+        assertEquals(true, containsDuplicate.containtDuplicateAlternate2(new int[]{5, 3, 1, 4, 3, 2}));
     }
 
     @Test
     public void testContainsDuplicate2() throws Exception {
         assertEquals(false, containsDuplicate.containsDuplicate(new int[]{1, 2, 3, 4, 5}));
         assertEquals(false, containsDuplicate.containtDuplicateAlternate(new int[]{1,2,3,4,5}));
+        assertEquals(true, containsDuplicate.containtDuplicateAlternate2(new int[]{5, 3, 1, 4, 3, 2}));
     }
 }
