f45829ba1926c73f30ff13c9808aff0d922b25d7
Anurag Kapur <anurag@beancrunch.com>
2019-12-22 14:41:13 +0000

    Simplify solution to srm-733-div2-250p problem

--- topcoder/SRM 773 DIV 2/src/ChristmasCrackers.java
+++ topcoder/SRM 773 DIV 2/src/ChristmasCrackers.java
@@ -2,49 +2,18 @@ import java.util.*;
 
 public class ChristmasCrackers {
 
-	Set<Integer> attendeeList = new HashSet<>();
-	List<Integer> peopleList = new ArrayList<>();
-	int originalK = 0;
-
 	public int[] crack(int N, int K) {
-		for (int i = 0; i < N; i++) {
-			attendeeList.add(i);
+		int totalElementsInResult = N * K;
+		if (totalElementsInResult % 2 != 0) {
+			totalElementsInResult ++;
 		}
-		originalK = K;
-		return crack(N, K, 0);
-	}
 
-	private int[] crack(int N, int K, int personIndex) {
-		if (done(peopleList)) {
-			return toIntArray(peopleList);
-		} else {
-			for (int i = 0; i < N; i = i + 2) {
-				if (done(peopleList)) {
-					break;
-				}
-				peopleList.add(personIndex % N);
-				personIndex ++;
-				peopleList.add(personIndex % N);
-				personIndex ++;
-			}
-			K --;
-			return crack(N, K, personIndex % N);
+		int[] result = new int[totalElementsInResult];
+
+		for (int i = 0; i < totalElementsInResult; i++) {
+			result[i] = i % N;
 		}
-	}
 
-	private boolean done(List<Integer> peopleList) {
-		for (Integer attendee : attendeeList) {
-			if (Collections.frequency(peopleList, attendee) < originalK) {
-				return false;
-			}
-		}
-		return true;
-	}
-
-	private int[] toIntArray(List<Integer> list) {
-		int[] ret = new int[list.size()];
-		for(int i = 0;i < ret.length;i++)
-			ret[i] = list.get(i);
-		return ret;
+		return result;
 	}
 }
