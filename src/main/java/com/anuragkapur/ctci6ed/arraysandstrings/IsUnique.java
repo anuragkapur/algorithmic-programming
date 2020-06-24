c9c1a739c4b03fd44b12cf9768c487bab6f63916
Anurag Kapur <anurag@beancrunch.com>
2020-06-24 07:52:19 +0100

    Add 1.1 IsUnique solution

---
+++ src/main/java/com/anuragkapur/ctci6ed/arraysandstrings/IsUnique.java
@@ -0,0 +1,55 @@
+package com.anuragkapur.ctci6ed.arraysandstrings;
+
+import java.io.*;
+import java.util.*;
+
+class IsUnique {
+
+    /**
+     * O(n)
+     *
+     * @param input
+     * @return
+     */
+    private static boolean isUnique(String input) {
+
+        if (input == null || input.isEmpty()) {
+            throw new IllegalArgumentException("Input string should not be null or empty");
+        }
+
+        Set<Character> uniqueCheckSet = new HashSet<Character>();
+        for(char ch : input.toCharArray()) {
+            boolean wasNumberAdded = uniqueCheckSet.add(ch);
+            if (!wasNumberAdded) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static boolean isUniqueNoAdditionalDataStructures(String input) {
+
+        if (input == null || input.isEmpty()) {
+            throw new IllegalArgumentException("Input string should not be null or empty");
+        }
+
+        char[] chs = input.toCharArray();
+        for (int i=0; i<chs.length; i++) {
+            char ch = chs[i];
+            for (int j=i+1; j<chs.length; j++) {
+                if (chs[j] == ch) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String input = "abc";
+        System.out.println(isUnique(input));
+        System.out.println(isUniqueNoAdditionalDataStructures(input));
+    }
+}
