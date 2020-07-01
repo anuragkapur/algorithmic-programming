fc7c9a9aef93c2956de7a57579eff01877300a31
Anurag Kapur <anurag@beancrunch.com>
2020-07-01 16:03:19 +0100

    Add FirstMissingInteger solution

---
+++ src/main/java/com/anuragkapur/ib/FirstMissingInteger.java
@@ -0,0 +1,64 @@
+package com.anuragkapur.ib;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+public class FirstMissingInteger {
+
+    /**
+     * RTC: O(n)
+     * SC: O(1)
+     *
+     * @param A
+     * @return
+     */
+    public int firstMissingPositive(int[] A) {
+
+        for (int i=0; i<A.length; i++) {
+            if (A[i] > 0 && A[i] <= A.length) {
+                int num = A[i];
+                if (num == i+1 || num == A[num-1]) {
+                    // number in correct pos, do nothing
+                } else {
+                    // swap
+                    A[i] = A[num-1];
+                    A[num-1] = num;
+                    // decrement counter to check the new element in ith pos in next iter
+                    i--;
+                }
+            }
+        }
+
+        for (int i=0; i<A.length; i++) {
+            if (A[i] != i+1) {
+                return i+1;
+            }
+        }
+
+        return A.length+1;
+    }
+
+    /**
+     * RTC: O(n)
+     * SC: O(n)
+     *
+     * @param A
+     * @return
+     */
+    public int firstMissingPositive1(ArrayList<Integer> A) {
+
+        Set<Integer> b = new HashSet<>();
+        for (Integer num : A) {
+            b.add(num);
+        }
+
+        int i = 1;
+        while (true) {
+            if (b.add(i)) {
+                return i;
+            }
+            i++;
+        }
+    }
+}
