2faf3d8708cabd274750df6a388d3b359d36de73
Anurag Kapur <anurag@beancrunch.com>
2020-08-01 12:10:42 +0100

    Use XOR to count different bits

--- src/main/java/com/anuragkapur/ib/bitmanipulation/DifferentBitsSumPairwise.java
+++ src/main/java/com/anuragkapur/ib/bitmanipulation/DifferentBitsSumPairwise.java
@@ -14,34 +14,24 @@ public class DifferentBitsSumPairwise {
         long sum = 0;
         for (int i=0; i<A.size(); i++) {
             for (int j=i+1; j<A.size(); j++) {
-                if (i != j) {
-                    int interimSum = f(A.get(i), A.get(j));
-                    interimSum *= 2;
-                    sum += interimSum;
-                    sum = sum % 1000000007;
-                }
+                int interimSum = f(A.get(i), A.get(j));
+                interimSum *= 2;
+                sum += interimSum;
+                sum = sum % 1000000007;
             }
         }
         return (int) sum;
     }
 
     private int f(Integer a, Integer b) {
-        String s1 = Integer.toBinaryString(a);
-        String s2 = Integer.toBinaryString(b);
-
-        if (s1.length() < s2.length()) {
-            s1 = padShorterString(s1, s2);
-        } else if (s2.length() < s1.length()) {
-            s2 = padShorterString(s2, s1);
-        }
-
+        int xor = a ^ b;
+        String xorString = Integer.toBinaryString(xor);
         int diffCount = 0;
-        for (int i=0; i<s1.length(); i++) {
-            if (s1.charAt(i) != s2.charAt(i)) {
+        for (int i = 0; i < xorString.length(); i++) {
+            if (xorString.charAt(i) == '1') {
                 diffCount ++;
             }
         }
-
         return diffCount;
     }
 
