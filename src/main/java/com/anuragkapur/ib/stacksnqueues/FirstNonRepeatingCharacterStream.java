c9a3da96eaddd3ef930a68da4023ae184e2771d4
Anurag Kapur <anurag@beancrunch.com>
2020-09-04 17:04:36 +0100

    Add RTC for FirstNonRepeatingCharacterStream solution

--- src/main/java/com/anuragkapur/ib/stacksnqueues/FirstNonRepeatingCharacterStream.java
+++ src/main/java/com/anuragkapur/ib/stacksnqueues/FirstNonRepeatingCharacterStream.java
@@ -6,6 +6,10 @@ import java.util.Map;
 import java.util.Queue;
 
 public class FirstNonRepeatingCharacterStream {
+
+    /**
+     * RTC: O(n)
+     */
     public String solve(String A) {
 
         char[] chs = A.toCharArray();
