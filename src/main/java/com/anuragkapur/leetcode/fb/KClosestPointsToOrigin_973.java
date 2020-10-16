365d6546871fb3e0cb4ff173627f138cad4a4662
Anurag Kapur <anurag@beancrunch.com>
2020-10-16 17:19:37 +0100

    Add KClosestPointsToOrigin_973 solution

---
+++ src/main/java/com/anuragkapur/leetcode/fb/KClosestPointsToOrigin_973.java
@@ -0,0 +1,56 @@
+package com.anuragkapur.leetcode.fb;
+
+import java.util.*;
+
+public class KClosestPointsToOrigin_973 {
+
+    /**
+     * RTC: O(n*log(n))
+     * where,
+     *  n = number of points
+     */
+    public int[][] kClosest(int[][] points, int K) {
+
+        Map<Integer, List<Integer>> distanceToIndex = new HashMap<>();
+
+        int[] distances = new int[points.length];
+
+        // O(n)
+        for (int i=0; i<points.length; i++) {
+            distances[i] = getDistance(points[i]);
+            if (distanceToIndex.containsKey(distances[i])) {
+                distanceToIndex.get(distances[i]).add(i);
+            } else {
+                List<Integer> indexList = new ArrayList<>();
+                indexList.add(i);
+                distanceToIndex.put(distances[i], indexList);
+            }
+
+        }
+
+        // O(n*log(n))
+        Arrays.sort(distances);
+
+        // O(k)...O(n)
+        int[][] kClosest = new int[K][2];
+        for (int i=0; i<K; i++) {
+            List<Integer> indexList = distanceToIndex.get(distances[i]);
+            for (int j=0; j<indexList.size(); j++) {
+                if (j>0) {
+                    i++;
+                }
+                if (i<K) {
+                    int indexOfPoint = indexList.get(j);
+                    kClosest[i] = points[indexOfPoint];
+                }
+            }
+        }
+
+        return kClosest;
+
+    }
+
+    private int getDistance(int[] coordinates) {
+        return coordinates[0]*coordinates[0] + coordinates[1]*coordinates[1];
+    }
+}
